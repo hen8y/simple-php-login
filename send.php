a5dbce167e733a7bcbea2383cf6c132272e7f53a
Ogbonna Henry <66043917+hen8y@users.noreply.github.com>
2024-03-25 12:16:12 +0100

    Update send.php

--- send.php
+++ send.php
@@ -30,7 +30,7 @@ try {
     $mail->Host       = 'rbx105.truehost.cloud';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'info@henrydev.org';                     //SMTP username
-    $mail->Password   = 'emenike134';                               //SMTP password
+    $mail->Password   = 'xxxxx';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
